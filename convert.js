3421dbc2809f87a1794ad69b0e779cd18b628d35
Kemrode <66632429+kemrode@users.noreply.github.com>
2021-03-09 12:10:54 +0100

    <create>1000:scribe JS function

---
+++ convert.js
@@ -0,0 +1,58 @@
+
+//bring back the script to use jQuery
+var script = document.createElement('jQuery');
+script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+let validateBtn = document.querySelector('.btnValid');
+let xmlInput = document.querySelector('#link');
+
+validateBtn.addEventListener('click', ()=> {
+
+    if (xmlInput) {
+
+        xmlToJson(xmlInput);
+        console.log("où en suis-je maintenant ?");
+        
+    }
+    else {
+        console.log("lien vide");
+    }
+})
+
+
+xmlToJson = function(xml) {
+    var obj = {};
+    if (xml.nodeType == 1) {                
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { 
+        obj = xml.nodeValue;
+    }            
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof (obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof (obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    console.log(obj);
+    return obj;
+}
+    
+// var jsonText = JSON.stringify(xmlToJson(xmlDoc)); 
