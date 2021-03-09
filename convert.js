47c98b1c44f83f848e9451e9aa2a4f86c40836a5
Kemrode <66632429+kemrode@users.noreply.github.com>
2021-03-09 14:25:32 +0100

    <create>1000: end of dev

--- convert.js
+++ convert.js
@@ -6,53 +6,46 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 let validateBtn = document.querySelector('.btnValid');
-let xmlInput = document.querySelector('#link');
+let xmlInput = document.getElementById('link');
 
-validateBtn.addEventListener('click', ()=> {
-
-    if (xmlInput) {
-
-        xmlToJson(xmlInput);
-        console.log("où en suis-je maintenant ?");
-        
-    }
-    else {
-        console.log("lien vide");
-    }
-})
+xmlInput.addEventListener('change', handleFileSelect, false)
 
 
-xmlToJson = function(xml) {
-    var obj = {};
-    if (xml.nodeType == 1) {                
-        if (xml.attributes.length > 0) {
-            obj["@attributes"] = {};
-            for (var j = 0; j < xml.attributes.length; j++) {
-                var attribute = xml.attributes.item(j);
-                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-            }
-        }
-    } else if (xml.nodeType == 3) { 
-        obj = xml.nodeValue;
-    }            
-    if (xml.hasChildNodes()) {
-        for (var i = 0; i < xml.childNodes.length; i++) {
-            var item = xml.childNodes.item(i);
-            var nodeName = item.nodeName;
-            if (typeof (obj[nodeName]) == "undefined") {
-                obj[nodeName] = xmlToJson(item);
-            } else {
-                if (typeof (obj[nodeName].push) == "undefined") {
-                    var old = obj[nodeName];
-                    obj[nodeName] = [];
-                    obj[nodeName].push(old);
-                }
-                obj[nodeName].push(xmlToJson(item));
-            }
-        }
-    }
-    console.log(obj);
-    return obj;
-}
+var ExcelToJSON = function() {
+
+    console.log("je suis dans la fonction");
+    this.parseExcel = function(file) {
+        console.log("Kevin1");
+        var reader = new FileReader();
+        console.log(reader);
+
+        reader.onload = function(e) {
+            console.log("kevin2");
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+            type: 'binary'
+            });
+            workbook.SheetNames.forEach(function(sheetName) {
+                console.log("kevin3");
+            // Here is your object
+            var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+            var json_object = JSON.stringify(XL_row_object);
+            console.log(JSON.parse(json_object));
+            jQuery( '#xlx_json' ).val( json_object );
+            })
+        };
+
+        reader.onerror = function(ex) {
+            console.log(ex);
+        };
+
+        reader.readAsBinaryString(file);
+        };
+    };
+
+    function handleFileSelect(evt) {
     
-// var jsonText = JSON.stringify(xmlToJson(xmlDoc)); 
+    var files = evt.target.files; // FileList object
+    var xl2json = new ExcelToJSON();
+    xl2json.parseExcel(files[0]);
+}
